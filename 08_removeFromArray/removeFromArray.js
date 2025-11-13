1142eb65c94eabe3133c91bcfac675ef5380ae0c
Junjie <cjunjie09@gmail.com>
2025-11-13 15:39:15 +0800

    Completed Exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // return the elements not from ...args
+    return arr.filter(val => !args.includes(val));
 };
 
 // Do not edit below this line
