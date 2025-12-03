1b72b743c1d207e3b2649826531df70b19f564e2
Junjie <cjunjie09@gmail.com>
2025-12-03 16:06:15 +0800

    Completed Exercise 13 1. create helper for alphanumerical values 2. filter str containing alphanum 3. reverse cleaned str 4. compare clean and reverse str

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = (str) => {
+    // helper to check whether str
+    // is only alphanumeric
+    const alphaNum = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-};
+    const cleanStr = str
+        .toLowerCase()
+        .split("")
+        .filter(char => alphaNum.includes(char))
+        .join("")
+
+    const reversedStr = cleanStr.split("").reverse().join("");
+
+    return cleanStr === reversedStr;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
