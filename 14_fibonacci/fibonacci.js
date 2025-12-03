4d379b59adb22ab92c3ec6c980a56d955f163600
Junjie <cjunjie09@gmail.com>
2025-12-03 16:35:54 +0800

    Completed Exercise 14 1. sanitize input 2. create first 2 elements in fib seq 3. create the whole fib seq up till count,input 4. return arraycount

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,45 @@
-const fibonacci = function() {
+const fibonacci = (countArg) => {
+    // treat input as trash, so sanitize first
+    // START OF SANITIZATION
+    let count
+    if (typeof countArg !== "number") {
+        // if not number, parse it as an integer
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
+
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+
+    // END OF SANITIZATION
+
+    // INDEX
+    // 1, 2, 3, 4, 5, 6, 7,  8
+
+    // VALUE
+    // 1, 1, 2, 3, 5, 8, 13, 21
+
+    // let firstPrev = 1;
+    // let secondPrev = 0;
+
+    // for (let i = 2; i <= count; i++) {
+    //     let current = firstPrev + secondPrev;
+    //     secondPrev = firstPrev;
+    //     firstPrev = current;
+    // }
+
+    // return firstPrev;
+
+    const fib = [0, 1];
+    // create the full array of fib sequence until count
+    // insert element from the second index onwards
+    // loop will stop at count index
+    for (let i = 2; i <= count; i++) {
+        // add prev 2 values
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[count];
 
 };
 
